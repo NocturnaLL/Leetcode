64bf9f88e1da69488987afbda877b7b3039007a9
NocturnaLL <bfidans1@binghamton.edu>
2024-10-04 16:13:14 -0400

    updated LRU cache solution.

--- LRUcache.cpp
+++ LRUcache.cpp
@@ -1,38 +1,46 @@
 class LRUCache {
-public:
-    LRUCache(int capacity) : capacity_(capacity) {}
-
+    public:
+    LRUCache(int capacity) : capacity(capacity) {}
     int get(int key) {
-        if (cache_map_.find(key) == cache_map_.end()) {
+        auto it = cache.find(key);
+        if(it==cache.end()) // cant find it
             return -1;
-        } else {
-            // Move the accessed key to the front of the list (most recent)
-            cache_items_list_.splice(cache_items_list_.begin(), cache_items_list_, cache_map_[key]);
-            return cache_map_[key]->second;
-        }
-    }
-
+        lruList.erase(it->second.second);
+        lruList.push_front(key);
+        it->second.second = lruList.begin();  // More efficient update cache[key] = pair(it.second.first, lruList.begin());
+        return it->second.first;
+    }   
+    
     void put(int key, int value) {
-        if (cache_map_.find(key) != cache_map_.end()) {
-            // Key exists, update value and move it to the front
-            cache_map_[key]->second = value;
-            cache_items_list_.splice(cache_items_list_.begin(), cache_items_list_, cache_map_[key]);
-        } else {
-            // Key does not exist, insert a new item
-            if (cache_items_list_.size() == capacity_) {
-                // Cache is full, remove the least recently used item (back of the list)
-                int key_to_remove = cache_items_list_.back().first;
-                cache_items_list_.pop_back();
-                cache_map_.erase(key_to_remove);
-            }
-            // Insert the new key-value pair at the front of the list
-            cache_items_list_.emplace_front(key, value);
-            cache_map_[key] = cache_items_list_.begin();
+        auto it = cache.find(key);
+        if(it!=cache.end()){
+            //update the value;
+            lruList.erase(it->second.second); // safer lruList.erase(cache[key].second.second);
+            lruList.push_front(key);
+            cache[key] = {value, lruList.begin()}; // Cleaner and preferred cache[key] = pair(value,lruList.begin());
         }
-    }
+        else {
+            // Key doesn't exist, insert a new key-value pair
+            if (lruList.size() == capacity) {
+            // Cache is full, evict the least recently used key (back of the list)
+                int lruKey = lruList.back();
+                lruList.pop_back();                // Remove the least recently used element
+                cache.erase(lruKey);               // Remove it from the cache
+            }
+            lruList.push_front(key);               // Add new key to the front of the list
+            cache[key] = {value, lruList.begin()}; // Insert new key-value pair in the cache
+        }
 
-private:
-    int capacity_;
-    std::list<std::pair<int, int>> cache_items_list_;
-    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> cache_map_;
+    }
+    private: 
+        unordered_map<int,pair<int,list<int>::iterator>> cache;
+        list<int> lruList;
+        int capacity;
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
